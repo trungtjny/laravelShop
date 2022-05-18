242cf3e9521b285035980cd98961a9a7ffea0a22
unknown <tjny6789@gmail.com>
2022-05-18 22:43:46 +0700

    fix version 0 - 1.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,7 +21,8 @@ class DatabaseSeeder extends Seeder
         $input['email'] = 'admin2@localhost.com';
         $input['password'] = Hash::make('123456') ;
         User::create($input); 
+        DB::table('designs')->insert(['id'=>null,'banner'=> "banner.jpg","slider1" =>'slider1.png',"slider2" =>'slider2.png',"slider3" =>'slider3.png',"slogan"=>"BabyShop - Thế giới đồ chơi an toàn cho trẻ"]);
         $this->call(categories::class); 
-        $this->call(products::class);
+        /* $this->call(products::class); */
     }
 }
