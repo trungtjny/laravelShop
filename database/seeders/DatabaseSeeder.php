8972153038fd9365d5eb24c286ac6d3444f9bc8d
unknown <tjny6789@gmail.com>
2022-05-18 22:45:08 +0700

    fix version 0 - 1.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,6 @@ class DatabaseSeeder extends Seeder
         User::create($input); 
         DB::table('designs')->insert(['id'=>null,'banner'=> "banner.jpg","slider1" =>'slider1.png',"slider2" =>'slider2.png',"slider3" =>'slider3.png',"slogan"=>"BabyShop - Thế giới đồ chơi an toàn cho trẻ"]);
         $this->call(categories::class); 
-        /* $this->call(products::class); */
+        $this->call(products::class); 
     }
 }
