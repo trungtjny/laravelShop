19261fb2884a1ec471cd238c0976187ca93af6bc
unknown <tjny6789@gmail.com>
2022-05-19 01:08:21 +0700

    ssdfga

--- database/seeders/categories.php
+++ database/seeders/categories.php
@@ -14,16 +14,16 @@ class categories extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            ['id'=> null,'name'=>"Đồ chơi thông minh",'parent_id'=>0, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi vận động",'parent_id'=>0, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Xe đồ chơi trẻ em",'parent_id'=>0, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Bể bơi phao bơi",'parent_id'=>0, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi nấu ăn",'parent_id'=>1, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi búp bê",'parent_id'=>1, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi xếp hình lắp ráp",'parent_id'=>1, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi học tập",'parent_id'=>1, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi búp bê",'parent_id'=>1, "description"=>null,"active"=>1],
-            ['id'=> null,'name'=>"Đồ chơi búp bê",'parent_id'=>1, "description"=>null,"active"=>1],
+            ['id'=> 1,'name'=>"Đồ chơi thông minh",'parent_id'=>0, "description"=>null,"active"=>1],
+            ['id'=> 2,'name'=>"Đồ chơi vận động",'parent_id'=>0, "description"=>null,"active"=>1],
+            ['id'=> 3,'name'=>"Xe đồ chơi trẻ em",'parent_id'=>0, "description"=>null,"active"=>1],
+            ['id'=> 4,'name'=>"Bể bơi phao bơi",'parent_id'=>0, "description"=>null,"active"=>1],
+            ['id'=> 5,'name'=>"Đồ chơi nấu ăn",'parent_id'=>1, "description"=>null,"active"=>1],
+            ['id'=> 6,'name'=>"Đồ chơi búp bê",'parent_id'=>1, "description"=>null,"active"=>1],
+            ['id'=> 7,'name'=>"Đồ chơi xếp hình lắp ráp",'parent_id'=>1, "description"=>null,"active"=>1],
+            ['id'=> 8,'name'=>"Đồ chơi học tập",'parent_id'=>1, "description"=>null,"active"=>1],
+            ['id'=> 9,'name'=>"Đồ chơi búp bê",'parent_id'=>1, "description"=>null,"active"=>1],
+            ['id'=> 10,'name'=>"Đồ chơi búp bê",'parent_id'=>1, "description"=>null,"active"=>1],
         ]);
     }
 }
